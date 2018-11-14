59409955523416bcccb8b87ac988ba9ae577dc8b
alakijayo <alakijaayo@gmail.com>
2018-11-14 16:14:59 +0000

    Removed edits

--- server/implicit_grant/app.js
+++ server/implicit_grant/app.js
@@ -11,4 +11,4 @@ var express = require('express'); // Express web server framework
 var app = express();
 app.use(express.static(__dirname + '/public'));
 console.log('Listening on 8888');
-app.listen('https://radioroomserver.herokuapp.com/');
+app.listen(8888)
