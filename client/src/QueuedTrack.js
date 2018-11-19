8c28c4c2ba4dd3e127ea7c1df315e6daca839dcb
Jon Urry <jon@urry.me>
2018-11-19 10:14:47 +0000

    Show votes on queued tracks
    show the number of votes on the queued tracks

--- client/src/QueuedTrack.js
+++ client/src/QueuedTrack.js
@@ -3,7 +3,6 @@ import React from 'react';
 export default class QueuedTrack extends React.Component {
   constructor(props) {
     super(props);
-    this.track = props.track;
     this.onVote = this.onVote.bind(this);
   }
   onVote(uri, vote) {
@@ -12,28 +11,35 @@ export default class QueuedTrack extends React.Component {
   render() {
     return (
       <div className="queued-track">
-        {this.track.artwork ? (
+        {this.props.track.artwork ? (
           <img
-            src={this.track.artwork}
+            src={this.props.track.artwork}
             alt="album cover art"
             style={{ height: 150 }}
           />
         ) : (
           ''
         )}
-        {this.track.artist ? <div className="">{this.track.artist}</div> : ''}
-        {this.track.track ? <div className="">{this.track.track}</div> : ''}
-        {this.track.uri ? (
-          <button onClick={this.onVote.bind(this, this.track.uri, 1)}>
-            Vote Up
-          </button>
+        {this.props.track.artist ? (
+          <div className="">{this.props.track.artist}</div>
         ) : (
           ''
         )}
-        {this.track.uri ? (
-          <button onClick={this.onVote.bind(this, this.track.uri, -1)}>
-            Vote Down
-          </button>
+        {this.props.track.track ? (
+          <div className="">{this.props.track.track}</div>
+        ) : (
+          ''
+        )}
+        {this.props.track.uri ? (
+          <div>
+            <button onClick={this.onVote.bind(this, this.props.track.uri, 1)}>
+              Vote Up
+            </button>
+            <button onClick={this.onVote.bind(this, this.props.track.uri, -1)}>
+              Vote Down
+            </button>
+            Votes: {this.props.track.votes}
+          </div>
         ) : (
           ''
         )}
