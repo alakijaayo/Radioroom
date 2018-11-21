06d0581847647c0b49b5faf72fe8e6f8660ef55d
NicGiles <n.r.giles1@gmail.com>
2018-11-21 14:18:21 +0000

    First working feature test with cypress

--- client/cypress/integration/radioroom.spec.js
+++ client/cypress/integration/radioroom.spec.js
@@ -1,6 +1,10 @@
-describe ('login button'), () => {
-  it ('allows spotify user to log on', () => {
-
-
+describe('homepage', () => {
+  it('opens homepage and displays login', () => {
+    cy.visit("http://localhost:3000")
+    cy.contains('RadioRoom')
+    cy.contains('To get started, please login via Spotify')
+    cy.contains('Note: To enjoy the full experience of RadioRoom you need to have a Spotify Premium subscription')
   })
+
+
 })
