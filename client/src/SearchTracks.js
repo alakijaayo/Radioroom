1eea3a0428cb3d036ba8d185741196990d7410c7
Jon Urry <jon@urry.me>
2018-11-21 10:06:35 +0000

    animate removal of track from search list
    add posed to animate the list of tracks

--- client/src/SearchTracks.js
+++ client/src/SearchTracks.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import SearchTrack from './SearchTrack.js';
-//import './css/SearchTracks.css';
+import posed, { PoseGroup } from 'react-pose';
+
+const Item = posed.li({});
 
 export default class SearchTracks extends React.Component {
   render() {
     const { tracks } = this.props;
     return (
       <div className="tracks">
-        {tracks.map(track => (
-          <SearchTrack
-            key={track.id || 'unique_dummy_id'}
-            track={track}
-            addToPlaylist={this.props.addToPlaylist}
-            removeTrack={this.props.removeTrack}
-          />
-        ))}
+        {tracks && tracks.length > 0 ? (
+          <ul>
+            <PoseGroup>
+              {tracks.map(track => (
+                <Item key={track.id || 'unique_dummy_id'}>
+                  <SearchTrack
+                    key={track.id || 'unique_dummy_id'}
+                    track={track}
+                    addToPlaylist={this.props.addToPlaylist}
+                    removeTrack={this.props.removeTrack}
+                  />
+                </Item>
+              ))}
+            </PoseGroup>
+          </ul>
+        ) : (
+          ''
+        )}
       </div>
     );
   }
