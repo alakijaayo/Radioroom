9a13f83e73d75ab4bc6afbc7cfdf15b7ca5a637d
NicGiles <n.r.giles1@gmail.com>
2018-11-21 14:31:23 +0000

    Updated Cypress JSON to allow multi URL tests. Tested Login button follows correct path

--- client/cypress/integration/radioroom.spec.js
+++ client/cypress/integration/radioroom.spec.js
@@ -6,5 +6,10 @@ describe('homepage', () => {
     cy.contains('Note: To enjoy the full experience of RadioRoom you need to have a Spotify Premium subscription')
   })
 
+  it('opens homepage and displays login', () => {
+    cy.visit("http://localhost:3000")
+    cy.contains('Login to Spotify').click()
+    cy.url().should('eq', 'http://localhost:8888/')
+})
 
 })
