c41be06505f143ab834aa6d52fa4969fe89d3a61
Jon Urry <jon@urry.me>
2018-11-22 12:07:58 +0000

    Style now playing

--- client/src/NowPlaying.js
+++ client/src/NowPlaying.js
@@ -1,41 +1,41 @@
 import React from 'react';
 
-
 export default class NowPlaying extends React.Component {
   constructor(props) {
-     super(props);
-     this.track = props.track;
-     this.onSkip = this.onSkip.bind(this);
-   }
+    super(props);
+    this.track = props.track;
+    this.onSkip = this.onSkip.bind(this);
+  }
 
-   onSkip(track, skip) {
-     this.props.skip();
-   }
-
-
-
-
+  onSkip(track, skip) {
+    this.props.skip();
+  }
 
   render() {
-         console.log(this.props.nowPlaying)
+    console.log(this.props.nowPlaying);
     return (
       <div className="now-playing">
         <h2>Now Playing</h2>
         {this.props.nowPlaying.track !== '' ? (
           <div>
             <img
+              className="w-75"
               src={this.props.nowPlaying.albumArt}
               alt="album cover art"
-              style={{ height: 150 }}
+              style={{ maxWidth: 400 }}
             />
-            <div>{this.props.nowPlaying.track}</div>
-            <div>by {this.props.nowPlaying.artist}</div>
-            <div>
-           <button onClick={this.onSkip.bind(this, this.props.nowPlaying.uri, )}
-           disabled={this.props.skipEnable}>
-           Skip
-           </button>
-           </div>
+            <div style={{ marginTop: -100 }}>
+              <button
+                onClick={this.onSkip.bind(this, this.props.nowPlaying.uri)}
+                disabled={this.props.skipEnable}
+              >
+                Skip
+              </button>
+            </div>
+            <div style={{ marginTop: 60 }}>
+              <div>{this.props.nowPlaying.track}</div>
+              <div>by {this.props.nowPlaying.artist}</div>
+            </div>
           </div>
         ) : (
           <div>Silence...</div>
